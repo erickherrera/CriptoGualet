b0ebc5628e9c226e672798b933fc5f9051b5d6a8
Erick Herrera Cabrera <erickmherrera01@gmail.com>
2025-08-12 21:35:08 -0600

    UX Update

--- CriptoGualet.h
+++ CriptoGualet.h
@@ -3,7 +3,44 @@
 
 #pragma once
 
+#include <commctrl.h>
+#include <ctime>
+#include <fstream>
 #include <iostream>
+#include <map>
+#include <memory>
+#include <random>
+#include <sstream>
+#include <string>
+#include <vector>
 #include <windows.h>
 
-// TODO: Reference additional headers your program requires here.
+// Application constants
+#define ID_LOGIN_BUTTON 1001
+#define ID_REGISTER_BUTTON 1002
+#define ID_USERNAME_EDIT 1003
+#define ID_PASSWORD_EDIT 1004
+#define ID_CREATE_WALLET_BUTTON 1005
+#define ID_VIEW_BALANCE_BUTTON 1006
+#define ID_SEND_BUTTON 1007
+#define ID_RECEIVE_BUTTON 1008s
+#define ID_LOGOUT_BUTTON 1009
+
+// Application states
+enum class AppState { LOGIN_SCREEN, MAIN_WALLET };
+
+// User structure
+struct User {
+  std::string username;
+  std::string passwordHash;
+  std::string walletAddress;
+  std::string privateKey;
+};
+
+// Global variables
+extern AppState g_currentState;
+extern std::map<std::string, User> g_users;
+extern std::string g_currentUser;
+extern HWND g_mainWindow;
+extern HFONT g_titleFont;
+extern HFONT g_buttonFont;
